7098edc319e8278a66f7e9edea68a453418ce86d
TimDeng <milkybird98@outlook.com>
2018-02-04 18:33:00 +0800

    add delete frame

--- delete.lua
+++ delete.lua
@@ -2,18 +2,15 @@ require("luci.sys")
 
 m = Map("smarthust","Smart HUST","Smarter HUST")
 
-s = m:section(TypedSection,"input","Add new user")
+s = m:section(TypedSection,"input","Delete user")
 s.addremove = false
 s.anonymous = true
 
 macaddr = s:option(Value,"macaddr",translate("Mac Address"))
-name = s:option(Value,"username",translate("Username"))
-pass = s:option(Value,"password",translate("Password"))
-pass.password = true
 
 local apply = luci.http.formvalue("cbi.apply")
 if apply then
-        io.popen("/usr/smarthust/addnew.sh restart")
+        io.popen("/usr/smarthust/delete.sh restart")
 end
 
 return m
--- smarthust.lua
+++ smarthust.lua
@@ -1,5 +1,7 @@
 module("luci.controller.smarthust.smarthust",package.seeall)
 
 function index()
-        entry({"admin","services","smarthust"},cbi("sh/input"),_("Smart HUST"),1
+        entry({"admin","services","smarthust"},alias("admin","services","smarthust","input"),_("Smart HUST"),60)     
+        entry({"admin","services","smarthust","input"},cbi("sh/input"),_("Add new user"),10).leaf=true
+        entry({"admin","services","smarthust","delete"},cbi("sh/delete"),_("Delete user"),20).leaf=true
         end
