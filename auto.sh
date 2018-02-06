8d8811ab94891d6ecb8a1c9d6a31b96a1039c807
Yue Pan <zxc479773533@gmail.com>
2018-02-06 15:53:50 +0800

    bug--

--- auto.sh
+++ auto.sh
@@ -4,24 +4,28 @@ INITSLEEPTIME=1
 SLEEPTIME=5
 userpasswdfile="userpasswd.txt"
 macfile="/tmp/dhcp.leases"
-runpath="/root/down/runmentohust"
 
 sleep $INITSLEEPTIME
 
 while true
 do
-  i=`ps -ef | grep zsh | grep -v grep | wc -l`
+  i=`ps | grep mentohust | grep -v grep | wc -l`
   if [ $i -eq 0 ] ; then
     echo "Dropped!"
-    for userpasswd in `cat $userpasswdfile`
+    nowuser=`uci get mentohust.@mentohust[0].Username`
+    for userpasswd in $(cat $userpasswdfile)
     do
       mac=${userpasswd:0:17}
       if [ `cat $macfile | grep $mac | wc -l` -ne 0 ] ; then
         user=${userpasswd:18:10}
         passwd=${userpasswd:29}
+        if [ "$user" = "$nowuser" ] ; then
+          continue
+        fi
         uci set mentohust.@mentohust[0].Username="$user"
         uci set mentohust.@mentohust[0].Password="$passwd"
-        `sh $runpath &`
+        /root/down/runmentohust &
+        echo "Restart $user"
         sleep 10
         break
       fi
